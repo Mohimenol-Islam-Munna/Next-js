8b8066fc4bde7b8822f13d54c6bef889d429cd53
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2022-02-15 16:29:18 +0600

    Start pre-rendering and data fetching ::: static generation with data fetching

---
+++ pages/ssg/users.js
@@ -0,0 +1,48 @@
+const Users = ({ users }) => {
+  console.log("users list in component :::", users);
+
+  return (
+    <div style={{ width: "60%", margin: "10px auto" }}>
+      <h2
+        style={{
+          border: "1px solid palegreen",
+          padding: "10px",
+          borderRadius: "5px",
+        }}
+      >
+        Users List
+      </h2>
+      <div>
+        {users.map((user) => (
+          <div
+            key={user.id}
+            style={{
+              border: "1px solid salmon",
+              padding: "10px",
+              borderRadius: "5px",
+              margin: "10px 0px",
+            }}
+          >
+            <h4>User Name: {user.name}</h4>
+            <p>User Email: {user.email}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Users;
+
+export async function getStaticProps() {
+  const result = await fetch("https://jsonplaceholder.typicode.com/users");
+
+  const users = await result.json();
+
+  // show data on terminal
+  console.log("users list ::", users);
+
+  return {
+    props: { users },
+  };
+}
