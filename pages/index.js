2a5b491e4168371692e672fde30d0b9a30d0b92c
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2022-02-15 12:49:33 +0600

    routing with client side : navigate from ui

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,24 @@
-const Index = () => {
+import Link from "next/link";
+
+const Home = () => {
   return (
-    <div style={{textAlign: "center"}}>
+    <div style={{ textAlign: "center" }}>
       <h2>Home Page</h2>
+      <div>
+        <ul style={{ listStyle: "none" }}>
+          <Link href="/blog">
+            <a style={{ display: "block" }}>Blog</a>
+          </Link>
+          <Link href="/docs">
+            <a style={{ display: "block" }}>Docs</a>
+          </Link>
+          <Link href="/product">
+            <a style={{ display: "block" }}>Product</a>
+          </Link>
+        </ul>
+      </div>
     </div>
   );
 };
 
-export default Index;
+export default Home;
