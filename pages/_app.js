299fc57f21126f5b977550d7de4cecd3385bfd3b
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2022-02-16 17:50:13 +0600

    Out Of The Box ::: next js Head component

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import "../styles/globals.css";
@@ -10,6 +11,10 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
+      <Head>
+        <title>Ecommerce</title>
+        <meta name="Ecommerce Site" content="This is the Ecommerce Site"></meta>
+      </Head>
       <Header>Content</Header>
       <Component {...pageProps} />
       <Footer></Footer>
--- pages/about.js
+++ pages/about.js
@@ -1,21 +1,27 @@
+import Head from "next/head";
 import Footer from "../components/Footer";
 
 const About = () => {
-    return (
-      <div style={{textAlign: "center"}}>
+  return (
+    <>
+      <Head>
+        <title>About Page</title>
+        <meta name="About Page" content="This is the about page"></meta>
+      </Head>
+      <div style={{ textAlign: "center" }}>
         <h2>About Page</h2>
       </div>
-    );
-  };
-  
-  export default About;
+    </>
+  );
+};
 
-  About.getLayout = function pageLayout(page){
-    return (
-      <>
+export default About;
+
+About.getLayout = function pageLayout(page) {
+  return (
+    <>
       {page}
       <Footer />
-      </>
-    )
-  }
-  
+    </>
+  );
+};
