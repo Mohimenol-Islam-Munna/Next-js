9334086871bb5c9b0b6f22ba01eff73db99e1ec8
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2022-02-16 16:22:16 +0600

    Api Routes ::: nested routing

---
+++ pages/api/blog/index.js
@@ -0,0 +1,10 @@
+export default async function handler(req, res) {
+    res.json({
+      name: "Blog Apli",
+      campus: "Jugantor",
+      subject: "All",
+      session: "2015-26",
+      contact: "jugantor@gmail.com"
+    });
+  }
+  
---
+++ pages/api/contact.js
@@ -0,0 +1,10 @@
+export default async function handler(req, res) {
+    res.json({
+      name: "Mohimenol Islam Munna",
+      campus: "PUST",
+      subject: "CSE",
+      session: "2015-26",
+      contact: "01773100924"
+    });
+  }
+  
