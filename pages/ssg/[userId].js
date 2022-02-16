0a4f1294d09d5d017721734b8dc201cf7b4a75f1
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2022-02-16 10:17:29 +0600

    Pre-rendering and data featching : understanding fallback false, ture

--- pages/ssg/[userId].js
+++ pages/ssg/[userId].js
@@ -1,51 +1,57 @@
+import { getRouteRegex } from "next/dist/shared/lib/router/utils";
 import { useRouter } from "next/router";
 
 const UserDetails = ({ user }) => {
-    const router = useRouter();
+  const router = useRouter();
 
-    const { userId } = router.query;
+  const { userId } = router.query;
 
-    console.log("router object :::", router);
+  console.log("router object :::", router);
 
-    return (
-        <div style={{ width: "60%", margin: "10px auto" }}>
-            <h2>User Details</h2>
-            <div>
-                <h3>User Name: {user.name}</h3>
-                <p>User Address {user.address && user.address.city}</p>
-            </div>
-        </div>
-    );
+  if (router.isFallback) {
+    return <h3>Loading...</h3>;
+  }
+
+  return (
+    <div style={{ width: "60%", margin: "10px auto" }}>
+      <h2>User Details</h2>
+      <div>
+        <h3>User Name: {user.name}</h3>
+        <p>User Address {user.address && user.address.city}</p>
+      </div>
+    </div>
+  );
 };
 
 export default UserDetails;
 
 export async function getStaticPaths() {
-    return {
-        paths: [
-            { params: { userId: "1" } },
-            { params: { userId: "2" } },
-            { params: { userId: "3" } },
-        ],
-        fallback: false,
-    };
+  return {
+    paths: [
+      { params: { userId: "1" } },
+      { params: { userId: "2" } },
+      { params: { userId: "3" } },
+      { params: { userId: "4" } },
+    ],
+    fallback: true,
+  };
 }
 
 export async function getStaticProps(context) {
-    const { params } = context;
-    console.log("single user params ::", params);
+  const { params } = context;
+  console.log("single user params ::", params);
 
-    const result = await fetch(
-        `https://jsonplaceholder.typicode.com/users/${params.userId}`
-    );
+  const result = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${params.userId}`
+  );
 
-    const user = await result.json();
+  const user = await result.json();
 
-    console.log("signle user :: ", user);
+  console.log("signle user :: ", user);
 
-    return {
-        props: {
-            user: user,
-        },
-    };
+  return {
+    props: {
+      user: user,
+    },
+  };
 }
